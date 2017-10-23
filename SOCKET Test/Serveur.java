534bc869ea5a72c6f7e6c2d0478449cadbd247fa
LAsmaa <lahmer.asmaa@gmail.com>
2017-10-23 16:16:40 +0100

    TP donné

---
+++ SOCKET Test/Serveur.java
@@ -0,0 +1,30 @@
+import java.net.*;
+import java.io.*;
+
+public class Serveur{
+	public static void main (String args[])
+	{
+	int port = 8000;
+	Socket sock_service;
+	ServerSocket sock_ecoute;
+	try {
+		sock_ecoute = new ServerSocket (port);
+		while (true) {
+		//flux entrant 
+		sock_service = sock_ecoute.accept();
+		BufferedReader br =new BufferedReader(new 
+				InputStreamReader( sock_service.getInputStream()));
+		String chaine = br.readLine();
+		System.out.println("Reçu : " + chaine);
+
+		// flux sortant
+		PrintStream ps = new PrintStream (sock_service.getOutputStream());
+		ps.println(chaine+" du serveur N " + args[0]);
+		sock_service.close();
+		}	
+		} 
+		catch (IOException e){
+		System.out.println(e.getMessage());
+		} 
+	}
+}
