9778e7880517683438b61b8605d65c19a41fb031
LAsmaa <lahmer.asmaa@gmail.com>
2017-11-01 20:14:47 +0100

    joueur OP

--- SoUNO1.2/Joueur.java
+++ SoUNO1.2/Joueur.java
@@ -26,6 +26,10 @@ public class Joueur{
     this.joue=joue;
   }
 
+  public boolean getGagne(){
+    return this.gagne;
+  }
+
   public void setGagne(boolean gagne){
     this.gagne=gagne;
   }
@@ -61,28 +65,51 @@ public class Joueur{
     }return choix;
   }
 
-
-  public void jouer (boolean joue){
-    int choix = this.choix_jeux();
-    if (choix==1){
-      ArrayList<Carte> main_a_jouer = this.get_main_joueur();
-      choix= -1;
-      while (choix != 0){
-        System.out.println("Votre main est :\n");
-        int i = 1;
-        for (Carte temp: main_a_jouer){
-          System.out.println(i + " " + temp);
-        }
-        choix = 0;
+  public boolean verifie_choix_carte(Carte carte_choisie, Carte carte_table){
+    if ((carte_choisie.getCouleur() == carte_table.getCouleur()) ||
+      (carte_choisie.getNumero() == carte_table.getNumero())||
+      (carte_choisie.getPouvoir() == carte_table.getPouvoir()) ||
+      (carte_choisie.getPouvoir()=="JOKER") ||
+      (carte_choisie.getPouvoir()=="PLUS QUATRE")){
+        return true;
       }
+      else return false;
+  }
 
-    }
-
-
+  public Carte choix_carte(Carte carte_table){
+    Carte carte_choisie;
+    //A faire:
+    //Verifier si la carte déposéé est bonne...... --"
+    Scanner sc = new Scanner(System.in);
+    int choix;
+    do{
+      for (Carte temp: main_joueur){
+        int i = 0;
+        System.out.println("- " + i + " " + temp + "\n");
+        i++;
+      }
+      System.out.println("Que voulez vous jouer ? (Inserrez le numero a gauche de la carte) ");
+      choix= sc.nextInt();
+      carte_choisie = main_joueur.get(choix);
+    }while ((choix < 0 ) && (choix > main_joueur.size()) && (verifie_choix_carte(carte_choisie, carte_table)));
+    return carte_choisie;
   }
 
 
 
-
-
+  //retourner la carte poséé Si
+  // Une carte normale IZI la game continue
+  // Une carte NULL eut dire qu'il pioche une carte
+  // IZI GAME IZI LIFE BABY :^)
+  public Carte jouer_joueur (Carte carte_table){
+    Carte carte_deposee ;
+    int choix_j = this.choix_jeux();
+    if (choix_j==1)/*deposer carte*/{
+      ArrayList<Carte> main_a_jouer = this.get_main_joueur();
+      carte_deposee  = this.choix_carte(carte_table);
+    }else{
+      carte_deposee = null;
+    }
+    return carte_deposee;
+  }
 }
--- SoUNO1.2/Partie.java
+++ SoUNO1.2/Partie.java
@@ -1,10 +1,11 @@
 import java.util.*;
 
 public class Partie{
-  ArrayList<Joueur> joueurs_de_partie;
-  int tour;
-  Carte carte_sur_table;
-  Jeu_De_Cartes jeu_de_partie;
+  private ArrayList<Joueur> joueurs_de_partie;
+  private int tour;
+  private Carte carte_sur_table;
+  private Jeu_De_Cartes jeu_de_partie;
+  private boolean partie_en_cour;
 
 
   public Partie(){
@@ -12,13 +13,45 @@ public class Partie{
     this.jeu_de_partie = new Jeu_De_Cartes();
     this.tour = 0;
     this.carte_sur_table = this.jeu_de_partie.removeCarte();
+    this.partie_en_cour = true;
+  }
+
+  public ArrayList<Joueur> getListeJoueurs(){
+      return this.joueurs_de_partie;
   }
 
   public void addJoueur(Joueur joueur){
     this.joueurs_de_partie.add(joueur);
   }
 
+  public Carte getCarteSurTable(){
+    return this.carte_sur_table;
+  }
 
+  public void setCarteSurTable(Carte carte){
+    this.carte_sur_table = carte;
+  }
 
+  public void jouer_partie(){
+    //Boucle pour jouer tant qu'il n'y a pas de gagnant
+    //Fire boucler plusieurs fois sur la liste des joueurs
+    do {
+      //Boucle fait boucler une seule fois sur la liste des joueurs
+      for(Joueur temp: this.getListeJoueurs()){
+          Carte carte_deposee = temp.jouer_joueur(this.getCarteSurTable());
+          if (carte_deposee == null){
+            Carte carte_piochee = jeu_de_partie.removeCarte();
+            temp.add_carte_main_joueur(carte_piochee);
+          }else{
+            jeu_de_partie.addCarte(this.getCarteSurTable());
+            setCarteSurTable(carte_deposee);
+          }
+          if (temp.getGagne()){
+            partie_en_cour= false;
+            break;
+          }
+      }
+    }while (partie_en_cour);
+  }
 
 }
